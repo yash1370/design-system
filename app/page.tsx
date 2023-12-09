cba9d2147e7693764c20cef9e082cde1afbe7a9a
yash1370 <patelyash1018@gmail.com>
2023-12-09 15:36:47 +0530

    aa

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div className="flex items-center justify-center h-full">
       <div>
-        <h1 className="text-3xl font-semibold">Know More About Storybook</h1>
+        <h1 className="text-3xl font-semibold">Learn Know More About Storybook</h1>
         <Link href="https://desingsystem-v2.vercel.app/" target="_blank">
           <p className="pt-6 text-xl font-medium underline flex items-center justify-center">
             Please follow this Link
