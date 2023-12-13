db47482a696186f7e437a99b218ef3e7ff6e4294
yash1370 <patelyash1018@gmail.com>
2023-12-13 19:07:09 +0530

    add

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
     <div className="flex items-center justify-center h-full">
       <div>
         <h1 className="text-3xl font-semibold">Learn Know More About Storybook</h1>
-        <Link href="https://desingsystem-v2.vercel.app/" target="_blank">
+        <Link href="https://designsystem-v2.vercel.app" target="_blank">
           <p className="pt-6 text-xl font-medium underline flex items-center justify-center">
             Please follow this Link
             <span className="newWindow6 underline mt-[2px] ms-2"></span>
