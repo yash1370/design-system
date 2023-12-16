02f1bdc19ba9154d80b1cf2b6c42a0faea6eab85
yash1370 <patelyash1018@gmail.com>
2023-12-16 06:06:20 +0530

    changes

---
+++ .storybook/theme.ts
@@ -0,0 +1,17 @@
+import { create } from "@storybook/theming";
+
+export default create({
+  base: "light",
+
+  colorSecondary: "#0137fc",
+
+  // Form colors
+  inputBg: "#f9fafb",
+  inputBorder: "#d1d5db",
+  inputTextColor: "#111827",
+
+  brandTitle: "Design System",
+  brandUrl: "https://designsystem-v2.vercel.app/",
+  brandImage:
+    "https://i.ibb.co/CsdP83G/Design-System-1.png",
+});
--- app/head.tsx
+++ app/head.tsx
@@ -1,19 +1,20 @@
 import React from "react";
 
 const Head = () => {
-    return (
-        <>
-            <meta charSet="utf-8" />
-            <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-            <meta
-                name="viewport"
-                content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no"
-            />
-            <title>Storybook</title>
-            <meta name="description" content="Storybook App" />
-            <meta name="keywords" content="Storybook App" />
-        </>
-    );
+  return (
+    <>
+      <meta charSet="utf-8" />
+      <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+      <meta
+        name="viewport"
+        content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no"
+      />
+      <title>Storybook</title>
+      <link href="/favicon.ico" rel="icon" />
+      <meta name="description" content="Storybook App" />
+      <meta name="keywords" content="Storybook App" />
+    </>
+  );
 };
 
 export default Head;
